346c374d6dff13ccca8ffa298cb89c3fa49eed74
felixjaschul <felix@jaschul.de>
2026-07-15 11:45:42 +0200

    v1.0.6: fix keyring order, TERM=dumb

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -2,7 +2,7 @@ class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
   url "https://github.com/xt9y/msl/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "7dc248b4eaf5ee8acb12ffdcac7f8d1cccc0ca8e2b9f5e70a21b62af64f4b55b"
+  sha256 "0f4594e22347eb172b18662dd88a0b7fe883fc049682c9f5afbd2ca23d0cd8cb"
   license "MIT"
 
   depends_on "filosottile/musl-cross/musl-cross" => "with-aarch64"
