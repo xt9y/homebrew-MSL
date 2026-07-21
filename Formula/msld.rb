d6b228c0c73a75b8b28430e942feeb4f0653bc72
felixjaschul <felix@jaschul.de>
2026-07-21 14:17:07 +0200

    msl v1.6.2: correct tarball with Makefile fix and Version.swift

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,9 +1,8 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/4e02a3e472495b947fcdb6068c83f8c92ee565e0.tar.gz"
-  version "1.6.1"
-  sha256 "3c65a7b33765bcc6ac69f5cf8f1498a6faf518ecde39960db3ed6123c728a95c"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v1.6.2.tar.gz"
+  sha256 "9fe62ad0380b65dfc499be502e7e046e6b519ae2d78e25f34d46cbd9f84325b5"
   license "MIT"
 
   depends_on "zig"
