c21092ef1b2c1ce7cfca617b8d4a04553faca1f1
felixjaschul <felix@jaschul.de>
2026-07-21 02:34:06 +0200

    v1.5.11: fix VSOCK module loading with finit_module + decompressed .ko

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -2,7 +2,7 @@ class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
   url "https://github.com/xt9y/msl/archive/refs/tags/v1.5.11.tar.gz"
-  sha256 "d55e4b1740f1004b1d76a6b80da6963dbb8faceec62a8c92a6275290277b167b"
+  sha256 "909f59623f2e4e13920aab408449434e00cf997634f8fcd0114b1187491699dd"
   license "MIT"
 
   depends_on "zig"
