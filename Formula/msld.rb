076a28763721eeb96592113da733eeaad8dc1e1e
felixjaschul <felix@jaschul.de>
2026-07-22 16:26:08 +0200

    v0.7.22: user authentication with login, users, and session management

--- Formula/msld.rb
+++ Formula/msld.rb
@@ -1,8 +1,8 @@
 class Msld < Formula
   desc "Guest daemon for msl (macOS Subsystem for Linux)"
   homepage "https://github.com/xt9y/msl"
-  url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.21.tar.gz"
-  sha256 "c343344ffa81a6b6a4b960fbf0b99864fe38be5f56964e4933fb22cba6efd1bd"
+  url "https://github.com/xt9y/msl/archive/refs/tags/v0.7.22.tar.gz"
+  sha256 "b018e62a288e1f2d8f4fdf6102ce94bc658f7d3ea86a59205239baa56f0fd0dd"
   license "MIT"
 
   depends_on "zig"
